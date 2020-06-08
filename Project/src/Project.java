5fe2f9fa35b999d2080f644b5396073af985cfcb
Jeong-Ho-Joon <63996844+Jeong-Ho-Joon@users.noreply.github.com>
2020-06-08 14:19:17 +0900

    5th

--- Project/src/Project.java
+++ Project/src/Project.java
@@ -1,85 +1,58 @@
-import java.awt.Button;
-import java.awt.Choice;
-import java.awt.Dimension;
-import java.awt.Frame;
-import java.awt.Label;
-import java.awt.Toolkit;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
-public class Project extends WindowAdapter {
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-   public void windowClosing(WindowEvent e) {
-      System.exit(0);
-   }
+public class Project {
+	public static void main(String[] args) {
+		JFrame f = new JFrame("서울 맛집 검색");
+		JFrame f2 = new JFrame("서울 맛집 검색");
+		JButton btn = new JButton("검색!");
+		JButton btn2 = new JButton("뒤로가기");
+		String[] area = { "강남구", "강동구", "강서구", "강북구", "관악구", "광진구", "구로구", "금천구", "노원구", "동대문구", "도봉구", "동작구", "마포구",
+				"서대문구", "성동구", "성북구", "서초구", "송파구", "영등포구", "용산구", "양천구", "은평구", "종로구", "중구", "중랑구" };
+		String[] type = { "고기류", "디저트", "분식", "양식", "일식", "중식", "채식", "한식" };
+		
+		JComboBox box1 = new JComboBox(area);
+		JComboBox box2 = new JComboBox(type);
+		Container ct1 = f.getContentPane();
+		ct1.setLayout(new FlowLayout());
+		Container ct2 = f2.getContentPane();
+		ct2.setLayout(new FlowLayout());
 
-   public Project() {
-      Frame f = new Frame("서울 맛집 조회 프로그램");
-      Frame f2 = new Frame("?구 맛집 열람");
-      Button b = new Button("확인");
-      Label text = new Label("맛집을 찾으실 구역을 지정해주세요.");
-      f.setSize(500, 300);
-      f.setLayout(null);
-      f.addWindowListener(this);
+		f.add(box1);
+		f.add(box2);
+		f.add(btn);
+		btn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				f.setVisible(false);
+				f2.setVisible(true);
+			}
+		});
+		f.setSize(400, 300);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setLocationRelativeTo(null);
+		f.setResizable(false);
+		f.setVisible(true);
 
-      Toolkit tk = Toolkit.getDefaultToolkit();
-      Dimension screenSize = tk.getScreenSize();
-
-      Choice res = new Choice();
-      res.add("강남구");
-      res.add("강동구");
-      res.add("강서구");
-      res.add("강북구");
-      res.add("관악구");
-      res.add("광진구");
-      res.add("구로구");
-      res.add("금천구");
-      res.add("노원구");
-      res.add("동대문구");
-      res.add("도봉구");
-      res.add("동작구");
-      res.add("마포구");
-      res.add("서대문구");
-      res.add("성동구");
-      res.add("성북구");
-      res.add("서초구");
-      res.add("송파구");
-      res.add("영등포구");
-      res.add("용산구");
-      res.add("양천구");
-      res.add("은평구");
-      res.add("종로구");
-      res.add("중구");
-      res.add("중랑구");
-
-      res.setSize(100, 120);
-      res.setLocation(200, 100);
-      b.setSize(50, 30);
-      b.setLocation(225, 150);
-      text.setBounds(160, 20, 200 ,60);
-
-      f.add(res);
-      f.add(b);
-      f.add(text);
-      f.setLocation(screenSize.width / 2 - 150, screenSize.height / 2 - 100);
-      f.setVisible(true);
-      
-      b.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-               f2.setVisible(true);
-            }
-         });
-      f2.setSize(500, 300);
-         f2.setLocation(200, 100);
-         f2.setLayout(null);
-         f2.setLocation(screenSize.width / 2 - 150, screenSize.height / 2 - 100);
-         f2.addWindowListener(this);
-   }
-   
-   
-   public static void main(String[] args) {
-      new Project();
-   }
+		f2.setVisible(false);
+		f2.add(btn2);
+		btn2.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				f.setVisible(true);
+				f2.setVisible(false);
+			}
+		});
+		f2.setSize(500, 500);
+		f2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f2.setLocationRelativeTo(null);
+	}
 }
