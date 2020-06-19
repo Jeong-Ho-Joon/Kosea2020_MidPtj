e89273279cbdad8cffa185a73c1778b627ad4aa0
Jeong-Ho-Joon <63996844+Jeong-Ho-Joon@users.noreply.github.com>
2020-06-19 13:10:15 +0900

    FINAL

--- SDSA/src/DAO.java
+++ SDSA/src/DAO.java
@@ -31,6 +31,8 @@ public class DAO {
 		return con;
 	}
 
+	//일반검색 쿼리문
+	
 	public Vector getList(String che1, String che2) {
 		Vector data = new Vector();
 
@@ -70,8 +72,10 @@ public class DAO {
 		}
 		return data;
 	}
-
-	public Vector getList() {
+	
+	//전체조회 쿼리문
+	
+	public Vector getList1() {
 
 		Vector data = new Vector();
 
@@ -82,9 +86,52 @@ public class DAO {
 		try {
 
 			con = getConn();
-			String sql2 = "select * from Restaurant order by Div";
-			System.out.println(sql2);
-			ps = con.prepareStatement(sql2);
+			String sql = "select * from Restaurant order by Div";
+			System.out.println(sql);
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				int NO = rs.getInt("NO");
+				String Div = rs.getString("Div");
+				String Name = rs.getString("Name");
+				String Location = rs.getString("Location");
+				String Tel = rs.getString("Tel");
+				String Mainmenu = rs.getString("Mainmenu");
+				String RF = rs.getString("RF");
+
+				Vector row = new Vector();
+				row.add(NO);
+				row.add(Div);
+				row.add(Name);
+				row.add(Location);
+				row.add(Tel);
+				row.add(Mainmenu);
+				row.add(RF);
+
+				data.add(row);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return data;
+	}
+	
+	//랜덤조회 쿼리문
+	
+	public Vector getList2() {
+
+		Vector data = new Vector();
+
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+
+			con = getConn();
+			String sql = "SELECT * FROM ( SELECT * FROM Restaurant ORDER BY DBMS_RANDOM.RANDOM ) WHERE ROWNUM < 15";
+			System.out.println(sql);
+			ps = con.prepareStatement(sql);
 			rs = ps.executeQuery();
 			while (rs.next()) {
 				int NO = rs.getInt("NO");
--- SDSA/src/Project.java
+++ SDSA/src/Project.java
@@ -38,6 +38,8 @@ public class Project {
 		btn2.setFont(new Font("배찌체", Font.BOLD, 20));
 		JButton btn3 = new JButton("뒤로가기");
 		btn3.setFont(new Font("배찌체", Font.BOLD, 20));
+		JButton btn4 = new JButton("랜덤조회");
+		btn4.setFont(new Font("배찌체", Font.BOLD, 20));
 		ImageIcon imageIcon1 = new ImageIcon("C:\\Users\\user\\Desktop\\사진\\chinese-icon-orange.png");
 
 		String[] area = { "강남구", "강동구", "강서구", "강북구", "구로구", "동대문구", "마포구", "서초구", "송파구", "영등포구", "종로구" };
@@ -54,6 +56,7 @@ public class Project {
 		f1.add(box2);
 		f1.add(btn1);
 		f1.add(btn2);
+		f1.add(btn4);
 		f1.add(lb1);
 		f1.add(lb2);
 		imagelb1.setIcon(new ImageIcon("C:\\Users\\user\\Desktop\\사진\\8.jpg"));
@@ -67,8 +70,10 @@ public class Project {
 		btn1.setBorderPainted(false);
 //		btn1.setContentAreaFilled(false);
 //		btn1.setFocusPainted(false);
-		btn2.setBounds(10, 325, 460, 25);
+		btn2.setBounds(270, 325, 200, 25);
 		btn2.setBorderPainted(false);
+		btn4.setBounds(10, 325, 200, 25);
+		btn4.setBorderPainted(false);
 
 		cols = getColumn();
 		model = new DefaultTableModel(cols, 0) {
@@ -115,7 +120,34 @@ public class Project {
 				System.out.println(boxset1);
 				String boxset2 = (String) box2.getSelectedItem();
 				DAO dao = new DAO();
-				v = dao.getList();
+				v = dao.getList1();
+				System.out.println(boxset2);
+				System.out.println("v=" + v);
+				model.setDataVector(v, cols);
+
+				tab.getColumnModel().getColumn(0).setPreferredWidth(0);
+				tab.getColumnModel().getColumn(1).setPreferredWidth(0);
+				tab.getColumnModel().getColumn(2).setPreferredWidth(50);
+				tab.getColumnModel().getColumn(3).setPreferredWidth(250);
+				tab.getColumnModel().getColumn(4).setPreferredWidth(50);
+				tab.getColumnModel().getColumn(5).setPreferredWidth(100);
+				tab.getColumnModel().getColumn(6).setPreferredWidth(250);
+
+				f2.add(pane);
+				f1.setVisible(false);
+				f2.setVisible(true);
+			}
+		});
+		
+		btn4.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				model.setNumRows(0);
+				String boxset1 = (String) box1.getSelectedItem();
+				System.out.println(boxset1);
+				String boxset2 = (String) box2.getSelectedItem();
+				DAO dao = new DAO();
+				v = dao.getList2();
 				System.out.println(boxset2);
 				System.out.println("v=" + v);
 				model.setDataVector(v, cols);
@@ -158,7 +190,7 @@ public class Project {
 				f2.setVisible(false);
 			}
 		});
-		
+
 		f2.setIconImage(image);
 		f2.setSize(1300, 400);
 		f2.setVisible(false);
